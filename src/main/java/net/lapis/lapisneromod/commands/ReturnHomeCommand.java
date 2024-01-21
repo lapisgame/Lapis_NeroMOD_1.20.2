240f04bb95b16fb8830fc887b730bb701ab7924d
lapis <klimantovartem@gmail.com>
2024-01-21 15:45:27 +0300

    Home Command

---
+++ src/main/java/net/lapis/lapisneromod/commands/ReturnHomeCommand.java
@@ -0,0 +1,37 @@
+package net.lapis.lapisneromod.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.lapis.lapisneromod.LapisNeroMOD;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+
+
+public class ReturnHomeCommand {
+    public ReturnHomeCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(Commands.literal("home").executes((command) -> {
+            return Home(command);
+        }));
+    }
+
+    private int Home(CommandContext<CommandSourceStack> context) throws CommandSyntaxException{
+        Player player = context.getSource().getPlayerOrException();
+
+        boolean hasHomepos = player.getPersistentData().getIntArray(LapisNeroMOD.MOD_ID + player.getId() + "homepos").length != 0;
+
+        if (hasHomepos){
+            int[] playerPos = player.getPersistentData().getIntArray(LapisNeroMOD.MOD_ID + player.getId() + "homepos");
+            player.moveTo( playerPos[0], playerPos[1], playerPos[2]);
+
+            player.displayClientMessage(Component.literal("Return Home!"), true);
+            return 1;
+        }
+        else{
+            player.displayClientMessage(Component.literal("No Home Position!"), true);
+            return -1;
+        }
+    }
+}
